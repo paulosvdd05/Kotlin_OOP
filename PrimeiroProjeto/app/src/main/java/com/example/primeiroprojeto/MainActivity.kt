e497edf6a3591d5dad5f683b2fdcaecb0be2af32
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-06 17:24:15 -0300

    atribuindo o valor do textView para ser igual ao editText

--- PrimeiroProjeto/app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
+++ PrimeiroProjeto/app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button
+        button.setOnClickListener{
+            textView.text = editText.text
+        }
     }
 }
