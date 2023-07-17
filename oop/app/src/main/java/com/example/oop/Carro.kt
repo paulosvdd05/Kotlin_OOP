f748912cbe410d5f5982e58b9d3a4c5a2c8a6376
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-17 17:29:28 -0300

    Criando classe carro e chamando funcao anda.

---
+++ oop/app/src/main/java/com/example/oop/Carro.kt
@@ -0,0 +1,18 @@
+package com.example.oop
+
+class Carro {
+
+    var cor: String? = null
+
+    fun anda(){
+        println("Carro andando: Vruum Vruuuumm!")
+    }
+
+    fun freia(){
+        println("Freiando o Carro")
+    }
+
+    fun buzina(){
+        println("^@*^HU@^@&^&(*J")
+    }
+}
--- oop/app/src/main/java/com/example/oop/main.kt
+++ oop/app/src/main/java/com/example/oop/main.kt
@@ -2,15 +2,6 @@ package com.example.oop
 
 
 fun main(){
-    val paulo = Human()
-    paulo.name = "Paulo Dias"
-    val dog = Dog()
-    dog.owner = paulo
-    dog.name = "Linux"
-    dog.sayHi()
-
-    val lucas = Human()
-    lucas.name = "Lucas"
-    dog.owner = lucas
-    dog.sayHi()
+    val carro = Carro()
+    carro.anda()
 }
