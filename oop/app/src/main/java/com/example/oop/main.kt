177376684b1dd28f98828a4314c21385ead4daa4
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-17 17:03:26 -0300

    Reutilizando a classe dog

--- oop/app/src/main/java/com/example/oop/main.kt
+++ oop/app/src/main/java/com/example/oop/main.kt
@@ -5,4 +5,8 @@ fun main(){
     val dog = Dog()
     dog.name = "Linux"
     dog.sayHi(ownerName = "Paulo Dias")
+
+    val dogLeticia = Dog()
+    dogLeticia.name = "Android"
+    dogLeticia.sayHi(ownerName = "Leticia" )
 }
