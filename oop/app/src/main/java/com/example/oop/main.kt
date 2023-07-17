fd89a08727f684aee4c4cd13b784741837242431
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-17 16:35:50 -0300

    Exemplos de variaveis em kotlin

--- oop/app/src/main/java/com/example/oop/main.kt
+++ oop/app/src/main/java/com/example/oop/main.kt
@@ -1,5 +1,15 @@
 package com.example.oop
 
+
+
 fun main(){
-    println("Ola mundo")
+    //variavel de apenas leitura(const js)
+    val name: String = "Paulo Dias"
+    //variavel em que o valor pode ser alterado(let js)
+    var idade: Int?
+    idade = 32
+    println("Ola $name $idade")
+    idade = 17
+    println("Ola $name $idade")
+
 }
